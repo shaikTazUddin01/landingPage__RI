b612cc3ad94f4c298ff53aa1ef188be67a34f917
taz <tazahmedsoft@gmail.com>
2025-03-11 09:25:16 +0600

    project setup

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSlider from './HeroSlider';
+
+
+
+const Hero = () => {
+  return (
+    <section className="w-full">
+      <h1 className='text-3xl font-semibold text-center'>Helmate</h1>
+      <HeroSlider />
+    </section>
+  );
+};
+
+export default Hero; 
---
+++ src/components/Hero/HeroSlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSlider = () => {
+  return (
+    <div>
+      <h1>this is react silder</h1>
+    </div>
+  );
+};
+
+export default HeroSlider;
