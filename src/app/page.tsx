3cda8c834105c1fdb74c130c961ff1123af7498f
taz <tazahmedsoft@gmail.com>
2025-04-13 00:20:59 +0600

    added delivery section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import ProductSection from "@/components/Products/Products";
 import ReviewSection from "@/components/Review/Reviews";
 import OrderSection from "@/components/OrderForm/OrderForm";
 import About from "@/components/About/about";
+import Delivery from "@/components/Delivery/Delivery";
 
 export default function Page() {
   return (
     <div>
      <HeroSection/>
      <About/>
+     <Delivery/>
      <ProductSection/>
      <ReviewSection/>
      <OrderSection/>
