4fc67a22f072c87d34fdfba594ff5869b13beb84
taz <tazahmedsoft@gmail.com>
2025-04-13 00:11:52 +0600

    added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import HeroSection from "@/components/Hero/Hero";
 import ProductSection from "@/components/Products/Products";
 import ReviewSection from "@/components/Review/Reviews";
 import OrderSection from "@/components/OrderForm/OrderForm";
+import About from "@/components/About/about";
 
 export default function Page() {
   return (
     <div>
      <HeroSection/>
+     <About/>
      <ProductSection/>
      <ReviewSection/>
      <OrderSection/>
